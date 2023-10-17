1420c52aa639ae24e140552437006c9f0622f038
John Adams <johnmadams96@gmail.com>
2023-10-17 11:45:57 -0400

    Created a test API

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -1,8 +1,21 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
 
 api = NinjaAPI()
+
+
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool
+    email: str = None
+    first_name: str = None
+    last_name: str = None
 
 
 @api.get("/hello")
 def hello(request):
     return "Hello world"
+
+
+@api.get("/me", response=UserSchema)
+def me(request):
+    return request.user
