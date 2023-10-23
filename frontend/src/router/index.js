a53a9de8c62e82565ab0cef597b1bda99cb9b4e0
John Adams <johnmadams96@gmail.com>
2023-10-23 15:44:29 -0400

    Added page skeletions

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import FourView from '../views/FourView.vue'
+import StoreView from '@/views/StoreView.vue'
+import ListView from '@/views/ListView.vue'
+import ItemView from '@/views/ItemView.vue'
 
 const routes = [
   {
@@ -9,12 +12,19 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/stores',
+    name: 'stores',
+    component: StoreView
+  },
+  {
+    path: '/lists',
+    name: 'lists',
+    component: ListView
+  },
+  {
+    path: '/items',
+    name: 'items',
+    component: ItemView
   },
   { 
     path: '/:catchAll(.*)', 
---
+++ frontend/src/views/StoreView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Shopping logo" src="/icons/favicon-128x128.png">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
