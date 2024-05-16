4c4e96eab8ea2e0ed42b98d2b4d4ee6479185e7c
John Adams <johnmadams96@gmail.com>
2024-05-16 10:53:42 -0400

    chore: enhanced admin portal

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,8 +3,58 @@ from .models import Store, Aisle, Item, ShoppingList, ListItem
 
 # Register your models here.
 
-admin.site.register(Store)
-admin.site.register(Aisle)
-admin.site.register(Item)
-admin.site.register(ShoppingList)
-admin.site.register(ListItem)
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+    search_fields = ["name"]
+
+    ordering = ["name"]
+
+
+class AisleAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "order", "store"]
+
+    list_filter = ["store"]
+
+    ordering = ["store", "order", "name"]
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "matches", "plural", "aisle"]
+
+    list_filter = ["aisle"]
+
+    ordering = ["name"]
+
+
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = ["id", "store", "name"]
+
+    list_filter = ["store"]
+
+    ordering = ["store", "name"]
+
+
+class ListItemAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "shopping_list",
+        "aisle",
+        "qty",
+        "item",
+        "purchased",
+        "purch_date",
+        "notes",
+    ]
+
+    list_filter = ["shopping_list", "aisle"]
+
+    ordering = ["shopping_list", "aisle", "item"]
+
+
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Aisle, AisleAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(ShoppingList, ShoppingListAdmin)
+admin.site.register(ListItem, ListItemAdmin)
