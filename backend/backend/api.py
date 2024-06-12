3082a2e49eab9afb8bfaaf3a7df89a89e5a376ee
John Adams <johnmadams96@gmail.com>
2024-06-12 15:01:46 -0400

    chore: change version to 1.5.3

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 
 api = NinjaAPI()
 api.title = "Shopping API"
-api.version = "1.5.2"
+api.version = "1.5.3"
 api.description = "API documentation for Shopping"
 
 
