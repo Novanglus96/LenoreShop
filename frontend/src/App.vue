b0e44229c5ba6e082419ac1e3c9cfb28215eeccd
John Adams <johnmadams96@gmail.com>
2024-08-07 14:59:31 -0400

    chore: update to version 1.5.8

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -50,7 +50,7 @@ const { prefetchVersion, version } = useVersion();
 const showBanner = ref(false);
 
 const checkVersion = computed(() => {
-  return version.value && version.value.version_number !== "1.5.6";
+  return version.value && version.value.version_number !== "1.5.8";
 });
 
 const updateBanner = () => {
