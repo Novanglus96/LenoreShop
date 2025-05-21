da111206cf057dfa0e4942216b177c0406a1dc0b
John Adams <johnmadams96@gmail.com>
2025-05-20 22:07:50 -0400

    enhancement: converted webpack to vite

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-import piniaPluginPersistedState from "pinia-plugin-persistedstate"
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
-import { VueQueryPlugin } from "@tanstack/vue-query"
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import piniaPluginPersistedState from "pinia-plugin-persistedstate";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 
-loadFonts()
+loadFonts();
 
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedState)
-const app = createApp(App)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState);
+const app = createApp(App);
 
-app.use(router)
-app.use(pinia)
-app.use(vuetify)
-app.use(VueQueryPlugin)
-app.mount('#app')
-  
+app.use(router);
+app.use(pinia);
+app.use(vuetify);
+app.use(VueQueryPlugin);
+app.mount("#app");
