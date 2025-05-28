5435e346c545433f45e66d4240fd17e3248dcaff
John Adams <johnmadams96@gmail.com>
2025-05-28 11:56:54 -0400

    chore: store form form validation and full screen

--- frontend/src/components/StoreForm.vue
+++ frontend/src/components/StoreForm.vue
@@ -1,81 +1,100 @@
 <template>
-  <v-dialog v-model="show" persistent width="1024">
+  <v-dialog
+    v-model="show"
+    persistent
+    :width="isMobile ? undefined : '1024'"
+    :fullscreen="isMobile"
+  >
     <v-card>
-      <v-card-title>
-        <span class="text-h5" v-if="props.isEdit == false">Add Store</span>
-        <span class="text-h5" v-else>Edit Store</span>
-      </v-card-title>
-      <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col cols="12" sm="6" md="4">
-              <v-text-field
-                label="Store Name*"
-                required
-                v-model="formData.name"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-        <small>*indicates required field</small>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="blue-darken-1" variant="text" @click="closeDialog">
-          Close
-        </v-btn>
-        <v-btn color="blue-darken-1" variant="text" @click="submitForm">
-          Save
-        </v-btn>
-      </v-card-actions>
+      <form @submit.prevent="submit">
+        <v-card-title>
+          <span class="text-h5" v-if="props.isEdit == false">Add Store</span>
+          <span class="text-h5" v-else>Edit Store</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="Store Name*"
+                  v-model="name.value.value"
+                  :error-messages="name.errorMessage.value"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue-darken-1" variant="text" @click="closeDialog">
+            Close
+          </v-btn>
+          <v-btn color="blue-darken-1" variant="text" type="submit">Save</v-btn>
+        </v-card-actions>
+      </form>
     </v-card>
   </v-dialog>
 </template>
 <script setup>
-import { ref, defineEmits, defineProps, onMounted, watchEffect } from "vue";
+  import { ref, defineEmits, defineProps, onMounted, watchEffect } from "vue";
+  import { useDisplay } from "vuetify";
+  import { useField, useForm } from "vee-validate";
 
-const formData = ref({
-  name: "",
-});
+  const { handleSubmit } = useForm({
+    validationSchema: {
+      name(value) {
+        if (value) return true;
 
-const props = defineProps({
-  storeFormDialog: {
-    type: Boolean,
-    default: false,
-  },
-  isEdit: {
-    type: Boolean,
-    default: false,
-  },
-  passedFormData: Array,
-});
-
-const show = ref(props.storeFormDialog);
-const emit = defineEmits(["addStore", "editStore", "updateDialog"]);
-
-const watchPassedFormData = () => {
-  watchEffect(() => {
-    if (props.passedFormData) {
-      formData.value.id = props.passedFormData.id;
-      formData.value.name = props.passedFormData.name;
-    }
+        return "Must provide a store name.";
+      },
+    },
   });
-};
 
-onMounted(() => {
-  watchPassedFormData();
-});
+  const name = useField("name");
+  const id = useField("id");
 
-const submitForm = async () => {
-  if (props.isEdit == false) {
-    emit("addStore", formData.value);
-  } else {
-    emit("editStore", formData.value);
-  }
-  closeDialog();
-};
+  const { smAndDown } = useDisplay();
+  const isMobile = smAndDown;
 
-const closeDialog = () => {
-  emit("updateDialog", false);
-};
+  const props = defineProps({
+    storeFormDialog: {
+      type: Boolean,
+      default: false,
+    },
+    isEdit: {
+      type: Boolean,
+      default: false,
+    },
+    passedFormData: Array,
+  });
+
+  const show = ref(props.storeFormDialog);
+  const emit = defineEmits(["addStore", "editStore", "updateDialog"]);
+
+  const watchPassedFormData = () => {
+    watchEffect(() => {
+      if (props.passedFormData) {
+        id.value.value = props.passedFormData.id;
+        name.value.value = props.passedFormData.name;
+      }
+    });
+  };
+
+  onMounted(() => {
+    watchPassedFormData();
+  });
+
+  const submit = handleSubmit(values => {
+    if (props.isEdit == false) {
+      emit("addStore", values);
+    } else {
+      emit("editStore", values);
+    }
+    closeDialog();
+  });
+
+  const closeDialog = () => {
+    emit("updateDialog", false);
+  };
 </script>
