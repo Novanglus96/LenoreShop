899aab9aa5664532e5271ec9465eb2a01831eafd
John Adams <johnmadams96@gmail.com>
2025-06-12 13:33:40 -0400

    docs: added documentation for ninja views

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -279,23 +279,78 @@ class ShoppingListFull(Schema):
 
 @api.get("/me", response=UserSchema)
 def me(request):
+    """
+    The function `me` returns a user.
+
+    Endpoint:
+        - **Path**: `/api/me`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+
+    Returns:
+        (UserSchema): Returns a user.
+    """
     return request.user
 
 
 @api.post("/aisles")
 def create_aisle(request, payload: AisleIn):
+    """
+    The function `create_aisle` creates an Aisle.
+
+    Endpoint:
+        - **Path**: `/api/aisles`
+        - **Method**: `POST`
+
+    Args:
+        request ():
+        payload (AisleIn): An object using schema of AisleIn.
+
+    Returns:
+        id (int): returns the id of the created Aisle.
+    """
     aisle = Aisle.objects.create(**payload.dict())
     return {"id": aisle.id}
 
 
 @api.post("/items", response=ItemOut)
 def create_item(request, payload: ItemIn):
+    """
+    The function `create_item` creates an Item.
+
+    Endpoint:
+        - **Path**: `/api/items`
+        - **Method**: `POST`
+
+    Args:
+        request ():
+        payload (ItemIn): An object using schema of ItemIn.
+
+    Returns:
+        id (int): returns the id of the created Item.
+    """
     item = Item.objects.create(**payload.dict())
     return item
 
 
 @api.post("/listitems")
 def create_listitem(request, payload: ListItemIn):
+    """
+    The function `create_listitem` creates a ListItem.
+
+    Endpoint:
+        - **Path**: `/api/listitems`
+        - **Method**: `POST`
+
+    Args:
+        request ():
+        payload (ListItemIn): An object using schema of ListItemIn.
+
+    Returns:
+        id (int): returns the id of the created ListItem.
+    """
     existing_item = ListItem.objects.filter(
         shopping_list_id=payload.shopping_list_id, item_id=payload.item_id
     ).first()
@@ -314,36 +369,120 @@ def create_listitem(request, payload: ListItemIn):
 
 @api.post("/shoppinglists")
 def create_shoppinglist(request, payload: ShoppingListIn):
+    """
+    The function `create_shoppinglist` creates a ShoppingList.
+
+    Endpoint:
+        - **Path**: `/api/shoppinglists`
+        - **Method**: `POST`
+
+    Args:
+        request ():
+        payload (ShoppingListIn): An object using schema of ShoppingListIn.
+
+    Returns:
+        id (int): returns the id of the created ShoppingList.
+    """
     shoppinglist = ShoppingList.objects.create(**payload.dict())
     return {"id": shoppinglist.id}
 
 
 @api.get("/aisles/{aisle_id}", response=AisleOut)
 def get_aisle(request, aisle_id: int):
+    """
+    The function `get_aisle` returns an Aisle.
+
+    Endpoint:
+        - **Path**: `/api/aisles/{aisle_id}`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+        aisle_id (int): An ID of an Aisle.
+
+    Returns:
+        (AisleOut): returns the Aisle object.
+    """
     aisle = get_object_or_404(Aisle, id=aisle_id)
     return aisle
 
 
 @api.get("/items/{item_id}", response=ItemOut)
 def get_item(request, item_id: int):
+    """
+    The function `get_item` returns an Item.
+
+    Endpoint:
+        - **Path**: `/api/items/{item_id}`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+        item_id (int): The ID of an Item.
+
+    Returns:
+        (ItemOut): returns an Item object.
+    """
     item = get_object_or_404(Item, id=item_id)
     return item
 
 
 @api.get("/listitems/{listitem_id}", response=ListItemOut)
 def get_listitem(request, listitem_id: int):
+    """
+    The function `get_listitem` returns a ListItem
+
+    Endpoint:
+        - **Path**: `/api/listitems/{listitem_id}`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+        listitem_id (int): The ID of a ListItem.
+
+    Returns:
+        (ListItemOut): returns a ListItem object.
+    """
     listitem = get_object_or_404(ListItem, id=listitem_id)
     return listitem
 
 
 @api.get("/shoppinglists/{shoppinglist_id}", response=ShoppingListOut)
 def get_shoppinglist(request, shoppinglist_id: int):
+    """
+    The function `get_shoppinglist` returns a ShoppingList.
+
+    Endpoint:
+        - **Path**: `/api/shoppinglists/{shoppinglist_id}`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+        shoppinglist_id (int): An ID of a ShoppingList.
+
+    Returns:
+        (ShoppingListOut): returns a ShoppingList object.
+    """
     shoppinglist = get_object_or_404(ShoppingList, id=shoppinglist_id)
     return shoppinglist
 
 
 @api.get("/shoppinglistfull/{shoppinglist_id}", response=ShoppingListFull)
 def get_shoppinglistfull(request, shoppinglist_id: int):
+    """
+    The function `get_shoppinglistfull` returns a ShoppingList with aisles and items.
+
+    Endpoint:
+        - **Path**: `/api/shoppinglistfull/{shoppinglist_id}`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+        shoppinglist_id (int): The ID of a ShoppingList.
+
+    Returns:
+        (ShoppingListFull): returns a ShoppingListFull object.
+    """
     shoppinglist = get_object_or_404(ShoppingList, id=shoppinglist_id)
     store = shoppinglist.store
     aisles = Aisle.objects.filter(
@@ -444,12 +583,40 @@ def get_shoppinglistfull(request, shoppinglist_id: int):
 
 @api.get("/aisles", response=List[AisleOut])
 def list_aisles(request):
+    """
+    The function `list_aisles` returns a list of Aisles.
+
+    Endpoint:
+        - **Path**: `/api/aisles`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+
+    Returns:
+        (List[AisleOut]): returns a list of Aisle objects.
+    """
     qs = Aisle.objects.all()
     return qs
 
 
 @api.get("/aislesbystore/{store_id}", response=List[AisleOut])
 def list_aislesbystore(request, store_id: int):
+    """
+    The function `list_aislesbystore` returns a list of Aisles for a matching
+    store ID.
+
+    Endpoint:
+        - **Path**: `/api/aislesbystore/{store_id}`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+        store_id (int): An ID of a Store.
+
+    Returns:
+        (List[AisleOut]): Returns a list of Aisles.
+    """
     qs = Aisle.objects.all().filter(store__id=store_id).order_by("order")
     return qs
 
@@ -461,6 +628,22 @@ def list_items(
     page_size: Optional[int] = Query(15),
     full: Optional[bool] = Query(False),
 ):
+    """
+    The function `list_items` returns a paginated list of Items.
+
+    Endpoint:
+        - **Path**: `/api/items`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+        page (int): The page number to return. Optional. Default = 1.
+        page_size (int): Hoe many items per page. Optional. Default = 15.
+        full (bool): Wehter this is a full request or not. Optional. Default = False.
+
+    Returns:
+        (PaginatedItems): returns a PaginatedItems object.
+    """
     qs = Item.objects.all().order_by("name")
     total_pages = 0
     item_list = []
@@ -484,24 +667,80 @@ def list_items(
 
 @api.get("/listitems", response=List[ListItemOut])
 def list_listitems(request):
+    """
+    The function `list_listitems` returns a list of ListItems.
+
+    Endpoint:
+        - **Path**: `/api/listitems`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+
+    Returns:
+        (List[ListItemOut]): Returns a list of ListItem objects.
+    """
     qs = ListItem.objects.all()
     return qs
 
 
 @api.get("/shoppinglists", response=List[ShoppingListOut])
 def list_shoppinglists(request):
+    """
+    The function `list_shoppinglists` returns a list of ShoppingLists.
+
+    Endpoint:
+        - **Path**: `/api/shoppinglists`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+
+    Returns:
+        (List[ShoppingListOut]): Returns a list of ShoppingList objects.
+    """
     qs = ShoppingList.objects.all()
     return qs
 
 
 @api.get("/listsbystore/{store_id}", response=List[ShoppingListOut])
 def list_listsbystore(request, store_id: int):
+    """
+    The function `list_listsbystore` returns a list of ShoppingLists for a given
+    Store ID.
+
+    Endpoint:
+        - **Path**: `/api/listsbystore/{store_id}`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+        store_id (int): The ID of a Store.
+
+    Returns:
+        (List[ShoppingListOut]): Returns a list of ShoppingList objects.
+    """
     qs = ShoppingList.objects.all().filter(store__id=store_id)
     return qs
 
 
 @api.put("/aisles/{aisle_id}")
 def update_aisle(request, aisle_id: int, payload: AisleIn):
+    """
+    The function `update_aisle` updates an Aisle
+
+    Endpoint:
+        - **Path**: `/api/aisles/{aisle_id}`
+        - **Method**: `PUT`
+
+    Args:
+        request ():
+        aisle_id (int): The ID of an aisle object.
+        payload (AisleIn): An Aisle object.
+
+    Returns:
+        success (bool): True if successfully updated.
+    """
     aisle = get_object_or_404(Aisle, id=aisle_id)
     aisle.name = payload.name
     aisle.order = payload.order
@@ -512,6 +751,21 @@ def update_aisle(request, aisle_id: int, payload: AisleIn):
 
 @api.put("/items/{item_id}")
 def update_item(request, item_id: int, payload: ItemIn):
+    """
+    The function `update_item` updates an Item.
+
+    Endpoint:
+        - **Path**: `/api/items/{item_id}`
+        - **Method**: `PUT`
+
+    Args:
+        request ():
+        item_id (int): ID of the item to update.
+        payload (ItemIn): An Item object with updates.
+
+    Returns:
+        success (bool): True if successfully updated.
+    """
     item = get_object_or_404(Item, id=item_id)
     item.name = payload.name
     item.matches = payload.matches
@@ -521,6 +775,21 @@ def update_item(request, item_id: int, payload: ItemIn):
 
 @api.put("/listitems/{listitem_id}")
 def update_listitem(request, listitem_id: int, payload: ListItemIn):
+    """
+    The function `update_listitem` updates a ListItem.
+
+    Endpoint:
+        - **Path**: `/api/listitems/{listitem_id}`
+        - **Method**: `PUT`
+
+    Args:
+        request ():
+        listitem_id (int): The ID of a ListItem to update.
+        payload (ListItemIn): A ListItem object with updates.
+
+    Returns:
+        success (bool): True if successfully updated.
+    """
     listitem = get_object_or_404(ListItem, id=listitem_id)
     listitem.qty = payload.qty
     listitem.purchased = payload.purchased
@@ -535,6 +804,21 @@ def update_listitem(request, listitem_id: int, payload: ListItemIn):
 
 @api.put("/shoppinglists/{shoppinglist_id}")
 def update_shoppinglist(request, shoppinglist_id: int, payload: ShoppingListIn):
+    """
+    The function `update_shoppinglist` updates a given ShoppingList.
+
+    Endpoint:
+        - **Path**: `/api/shoppinglists/{shoppinglist_id}`
+        - **Method**: `PUT`
+
+    Args:
+        request ():
+        shoppinglist_id (int): ID of the Shoppinglist to update.
+        payload (ShoppingListIn): A ShoppingList object with updates.
+
+    Returns:
+        success (bpp;): True if successfully updated.
+    """
     shoppinglist = get_object_or_404(ShoppingList, id=shoppinglist_id)
     shoppinglist.name = payload.name
     shoppinglist.store_id = payload.store_id
@@ -544,6 +828,20 @@ def update_shoppinglist(request, shoppinglist_id: int, payload: ShoppingListIn):
 
 @api.delete("/aisles/{aisle_id}")
 def delete_aisle(request, aisle_id: int):
+    """
+    The function `delete_aisle` deletes a given Aisle.
+
+    Endpoint:
+        - **Path**: `/api/aisles/{aisle_id}`
+        - **Method**: `DELETE`
+
+    Args:
+        request ():
+        aisle_id (int): ID of an Aisle to delete.
+
+    Returns:
+        success (bool): True if successfully deleted.
+    """
     aisle = get_object_or_404(Aisle, id=aisle_id)
     aisle.delete()
     return {"success": True}
@@ -551,6 +849,20 @@ def delete_aisle(request, aisle_id: int):
 
 @api.delete("/items/{item_id}")
 def delete_item(request, item_id: int):
+    """
+    The function `delete_item` deletes a given Item.
+
+    Endpoint:
+        - **Path**: `/api/items/{item_id}`
+        - **Method**: `DELETE`
+
+    Args:
+        request ():
+        item_id (int): ID of an Item to delete.
+
+    Returns:
+        success (bool): True if successfully deleted.
+    """
     item = get_object_or_404(Item, id=item_id)
     item.delete()
     return {"success": True}
@@ -558,6 +870,20 @@ def delete_item(request, item_id: int):
 
 @api.delete("/listitems/{listitem_id}")
 def delete_listitem(request, listitem_id: int):
+    """
+    The function `delete_listitem` deletes a given ListItem.
+
+    Endpoint:
+        - **Path**: `/api/listitems/{listitem_id}`
+        - **Method**: `DELETE`
+
+    Args:
+        request ():
+        listitem_id (int): ID of an ListItem to delete.
+
+    Returns:
+        success (bool): True if successfully deleted.
+    """
     listitem = get_object_or_404(ListItem, id=listitem_id)
     listitem.delete()
     return {"success": True}
@@ -565,6 +891,21 @@ def delete_listitem(request, listitem_id: int):
 
 @api.delete("/listitems/deleteall/{shoppinglist_id}")
 def delete_listitems_by_shoppinglist(request, shoppinglist_id: int):
+    """
+    The function `delete_listitems_by_shoppinglist` deletes all ListItems for
+    a given ShoppingList ID.
+
+    Endpoint:
+        - **Path**: `/api/listitems/deleteall/{shoppinglist_id}`
+        - **Method**: `DELETE`
+
+    Args:
+        request ():
+        shoppinglist_id (int): ID of a ShoppingList.
+
+    Returns:
+        success (bool): True if successfully deleted.
+    """
     listitems = ListItem.objects.filter(shopping_list_id=shoppinglist_id)
     listitems.delete()
     return {"success": True}
@@ -572,6 +913,21 @@ def delete_listitems_by_shoppinglist(request, shoppinglist_id: int):
 
 @api.delete("/listitems/deletepurchased/{shoppinglist_id}")
 def delete_purchased_listitems_by_shoppinglist(request, shoppinglist_id: int):
+    """
+    The function `delete_purchased_listitems_by_shoppinglist` deletes all ListItems
+    markded as purchased on a given ShoppingList.
+
+    Endpoint:
+        - **Path**: `/api/listitems/deletepurchased/{shoppinglist_id}`
+        - **Method**: `DELETE`
+
+    Args:
+        request ():
+        shoppinglist_id (int): ID of a ShoppingList.
+
+    Returns:
+        success (bool): True if successfully deleted.
+    """
     listitems = ListItem.objects.filter(
         shopping_list_id=shoppinglist_id, purchased=True
     )
@@ -581,6 +937,20 @@ def delete_purchased_listitems_by_shoppinglist(request, shoppinglist_id: int):
 
 @api.delete("/shoppinglists/{shoppinglist_id}")
 def delete_shoppinglist(request, shoppinglist_id: int):
+    """
+    The function `delete_shoppinglist` deletes a given ShoppingList.
+
+    Endpoint:
+        - **Path**: `/api/shoppinglists/{shoppinglist_id}`
+        - **Method**: `DELETE`
+
+    Args:
+        request ():
+        shoppinglist_id (int): ID of a ShoppingList to delete.
+
+    Returns:
+        success (bool): True if successfully deleted.
+    """
     shoppinglist = get_object_or_404(ShoppingList, id=shoppinglist_id)
     shoppinglist.delete()
     return {"success": True}
@@ -588,24 +958,80 @@ def delete_shoppinglist(request, shoppinglist_id: int):
 
 @api.post("/stores")
 def create_store(request, payload: StoreIn):
+    """
+    The function `create_store` creates a Store.
+
+    Endpoint:
+        - **Path**: `/api/stores`
+        - **Method**: `POST`
+
+    Args:
+        request ():
+        payload (StoreIn): A Store object to add.
+
+    Returns:
+        id (int): The ID of the added Store.
+    """
     store = Store.objects.create(**payload.dict())
     return {"id": store.id}
 
 
 @api.get("/stores/{store_id}", response=StoreOut)
 def get_store(request, store_id: int):
+    """
+    The function `get_store` returns a Store object for a given ID.
+
+    Endpoint:
+        - **Path**: `/api/stores/{store_id}`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+        store_id (int): ID of a Store to retreive.
+
+    Returns:
+        (StoreOut): A Store object.
+    """
     store = get_object_or_404(Store, id=store_id)
     return store
 
 
 @api.get("/stores", response=List[StoreOut])
 def list_stores(request):
+    """
+    The function `list_stores` returns a list of Stores.
+
+    Endpoint:
+        - **Path**: `/api/stores`
+        - **Method**: `GET`
+
+    Args:
+        request ():
+
+    Returns:
+        (List[StoreOut]): A list of Store objects.
+    """
     qs = Store.objects.all()
     return qs
 
 
 @api.put("/stores/{store_id}")
 def update_store(request, store_id: int, payload: StoreIn):
+    """
+    The function `update_store` updates a give Store.
+
+    Endpoint:
+        - **Path**: `/api/stores/{store_id}`
+        - **Method**: `PUT`
+
+    Args:
+        request ():
+        store_id (int): ID of a Store to update.
+        payload (StoreIn): A Store object with updates.
+
+    Returns:
+        success (bool): True if successfully updated.
+    """
     store = get_object_or_404(Store, id=store_id)
     store.name = payload.name
     store.save()
@@ -614,6 +1040,20 @@ def update_store(request, store_id: int, payload: StoreIn):
 
 @api.delete("/stores/{store_id}")
 def delete_store(request, store_id: int):
+    """
+    The function `delete_store` deletes a given Store.
+
+    Endpoint:
+        - **Path**: `/api/stores/{store_id}`
+        - **Method**: `DELETE`
+
+    Args:
+        request ():
+        store_id (int): ID of a Store to delete.
+
+    Returns:
+        success (bool): True if successfully deleted.
+    """
     store = get_object_or_404(Store, id=store_id)
     store.delete()
     return {"success": True}
